010021206065f3b2cbaa2e47b470057341ab2b44
katyabarkalova <guliaev.p2014@yandex.ru>
2022-06-21 21:17:10 +0300

    1
    1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,10 +26,15 @@ public class MainActivity extends AppCompatActivity {
                 "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
                 "Китти", "Масяня", "Симба"
         };
+        List<String> test = new ArrayList<>();
+        for (int i=1; i<30;i++){
+            test.add("test"+i);
+        }
+
 
 // используем адаптер данных
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, catNames);
+                android.R.layout.simple_list_item_1, test);
 
         listView.setAdapter(adapter);
     }
